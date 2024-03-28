811e8e278958e663139e676354982854d6472e93
Z4K201909 <zakalhabibi@gmail.com>
2024-03-28 02:29:54 -0500

    Update test_main.py

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import url_for
-from GasStationFrontend import create_app, db
+from __init__ import create_app, db
  # Change with actual flask app name
 from .models import User # Change with actual flask app name
 
