98805960742f21a7099324a0d53fb781838750dc
Khang <Khangduy2712@gmail.com>
2024-03-28 23:24:28 -0500

    validForm test

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 from backend import create_app
 from backend.models import db
+from backend.main import ProfileForm
 
 class TestMainBlueprint(unittest.TestCase):
 
@@ -28,7 +29,29 @@ class TestMainBlueprint(unittest.TestCase):
                 sess['user_id'] = 1  # Assuming user ID 1 exists in our database
         response = self.app.get('/profile')
         self.assertEqual(response.status_code, 302)  # Expect success
-    
+
+    def test_profileForm_validation(self):
+        with self.app.application.app_context():
+            form = ProfileForm()
+            form.first_name.data = 'John'
+            form.last_name.data = 'Wick'
+            form.username.data = 'MrWick'
+            form.email.data = "JW@continental.com"
+
+            # Wrong email type
+            form.email.data = "JW.com"
+            self.assertEqual(form.validate(), False)  
+            
+            # PW not match
+            form.password.data = 'HighTableSucks123'
+            form.confirmed_password.data = 'HighTableSucks456'
+            self.assertEqual(form.validate(), False)  
+
+            form.address_primary.data = '123 Continental St'
+            form.city.data = 'New York'
+            form.state.data = 'NY'
+            form.zipcode.data = '12345'
+            
     # Add more tests for other routes and functionalities as needed
 
 if __name__ == '__main__':
