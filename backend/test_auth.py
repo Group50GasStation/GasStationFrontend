2700b13f874b92fc648c5e064cf1c5f4b4fbe0ba
Noah M <noahcodes@protonmail.com>
2024-03-28 21:33:56 -0500

    Add basic validation for login form

--- backend/test_auth.py
+++ backend/test_auth.py
@@ -2,6 +2,7 @@ import unittest
 from flask import url_for
 from backend import create_app
 from backend.models import db
+from backend.auth import LoginForm
 
 
 class TestAuthBlueprint(unittest.TestCase):
@@ -19,11 +20,40 @@ class TestAuthBlueprint(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
+    # Tests below here -------------------
+
     def test_login_page_loads(self):
         response = self.app.get('/login')
         self.assertEqual(response.status_code, 200)  # Expect success
 
-    # Add more tests for login, registration, and logout functionalities as needed
+    def test_login_form_bad_email(self):
+        with self.app.application.app_context():
+            form = LoginForm()
+            form.email.data = "blah@something"
+            form.password.data = "password"
+            self.assertEqual(form.validate(), False)
+            form.email.data = "blah.com"
+            self.assertEqual(form.validate(), False)
+            form.email.data = "test@.com"
+            self.assertEqual(form.validate(), False)
+
+    def test_login_form_bad_password(self):
+        with self.app.application.app_context():
+            form = LoginForm()
+            form.email.data = "blah@something.com"
+            # Pwd too long
+            form.password.data = "wowdudethispasswordiswayyytoooooolongggggImeanwayyyytooooodamnlonggggggggggg"
+            self.assertEqual(form.validate(), False)
+            # Pwd too short
+            form.password.data = "no"
+            self.assertEqual(form.validate(), False)
+
+    def test_login_form_okay(self):
+        with self.app.application.app_context():
+            form = LoginForm()
+            form.email.data = "blah@something.com"
+            form.password.data = "password"
+            self.assertEqual(form.validate(), True)
 
 if __name__ == '__main__':
     unittest.main()
